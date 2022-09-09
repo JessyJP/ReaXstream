a6f152d246393e80a94fe3da68d83d3b4ee90ff2
JessyJP <49342341+JessyJP@users.noreply.github.com>
2022-09-09 23:12:39 +0300

    Update ReaXstreamFrames.cpp

--- Source/ReaXstreamFrames.cpp
+++ Source/ReaXstreamFrames.cpp
@@ -17,7 +17,7 @@ ReaStreamClassicFrame::ReaStreamClassicFrame
         unsigned int    in_audioSampleRate
     )
 {
-    for (int c = 0; c < 3; c++) { packetID[c] = in_packetID[c]; }//Good old for loop
+    for (int c = 0; c < 4; c++) { packetID[c] = in_packetID[c]; }//Good old for loop
     for (int c = 0; (c < 31) && (c < packetconnectionIdentifier.length()); c++) 
     { interconnectID[c] = packetconnectionIdentifier[c]; }//Good old for loop
     // The last byte interconnectID[31] = 0; should already be zero from the constructor
@@ -61,6 +61,24 @@ void ReaStreamClassicFrame::calculateMemIndices()
 //    }
 //}
 
+std::string ReaStreamClassicFrame::printFrameHeader()
+{
+    string sep = " ";
+    string outputString = "";
+
+    outputString += ("PacketID [") + string(packetID) + "] " + sep;
+    outputString += ("Interconnect ID: [") + string(interconnectID) + "] " + sep;
+    outputString += ("numAudioChannels: [") + std::to_string(numAudioChannels) + "] " + sep;
+    outputString += ("audioSampleRate: [") + std::to_string(audioSampleRate) + "] " + sep;
+    if (sampleByteSize > 0)
+    {
+        outputString += ("Packet Size: [") + std::to_string(packetSize) + "] " + sep;
+        outputString += ("sampleByteSize: [") + std::to_string(sampleByteSize) + "] " + sep;
+    }
+
+    return outputString;
+}
+
 void ReaStreamClassicFrame::packNextAudioBufferInRSframe(juce::AudioBuffer<float>& buffer, char* UDPbuffer, int audioSampleBuffInd, int samplesToRead)
 {
     // Increment the packet index
@@ -86,23 +104,7 @@ void ReaStreamClassicFrame::packNextAudioBufferInRSframe(juce::AudioBuffer<float
     }
 }
 
-std::string ReaStreamClassicFrame::printFrameHeader()
-{
-    string sep = " ";
-    string outputString = "";
 
-    outputString += ("PacketID [") + string(packetID) + "] " + sep;
-    outputString += ("Interconnect ID: [") + string(interconnectID) + "] " + sep;
-    outputString += ("numAudioChannels: [") + std::to_string(numAudioChannels) + "] " + sep;
-    outputString += ("audioSampleRate: [") + std::to_string(audioSampleRate) + "] " + sep;
-    if (sampleByteSize > 0)
-    {
-        outputString += ("Packet Size: [") + std::to_string(packetSize) + "] " + sep;
-        outputString += ("sampleByteSize: [") + std::to_string(sampleByteSize) + "] " + sep;
-    }
-
-    return outputString;
-}
 
 void ReaStreamClassicFrame::unpackUDPpayloadToRSframe(juce::AudioBuffer<float>& buffer, char* UDP_payload)
 {
